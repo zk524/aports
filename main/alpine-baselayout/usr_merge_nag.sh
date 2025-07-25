7968b7bd99a8675d8d6dffdf2adaaf3c3d11b9bc
Pablo Correa Gómez <pabloyoyoista@postmarketos.org>
2025-07-25 15:15:14 +0200

    main/alpine-baselayout: add warning for non-/usr-merged systems

---
+++ main/alpine-baselayout/usr_merge_nag.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+if [ ! -L /lib ] && [ "$1" = "post-commit" ]; then
+	cat << __EOF__
+* WARNING: The current system is not /usr-merged. You are encouraged to
+* migrate manually to ensure the best-possible support. See
+* https://alpinelinux.org/posts/2025-09-01-usr-merge.html for more details
+__EOF__
+fi
